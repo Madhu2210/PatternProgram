085bae6aaf948eb0e5e1a386f30b7fb3481fb0ea
Madhu kahar <madhukahar@gmail.com>
2022-04-21 14:10:59 +0530

    Added: Number Pattern Added

--- src/PatternProgram.java
+++ src/PatternProgram.java
@@ -1,9 +1,11 @@
 public class PatternProgram {
     public static void main(String[] args) {
-        int i,j, row = 6;
-        for (i=0; i<row; i++) {
+        int i, j, num, n = 7;
+        for (i=0; i<n; i++) {
+            num=1;
             for (j=0; j<=i; j++) {
-                System.out.print("* ");
+                System.out.print(num+ " ");
+                num++;
             }
             System.out.println();
         }
