574d82eaf0a955daee8f38f9cac680f396405e72
Madhu kahar <madhukahar@gmail.com>
2022-04-21 14:28:49 +0530

    Added: Added Star Pattern

--- src/PatternProgram.java
+++ src/PatternProgram.java
@@ -1,11 +1,9 @@
 public class PatternProgram {
     public static void main(String[] args) {
-        int i, j, num, n = 7;
-        for (i=0; i<n; i++) {
-            num=1;
+        int i, j, row=6;
+        for (i=0; i<row; i++) {
             for (j=0; j<=i; j++) {
-                System.out.print(num+ " ");
-                num++;
+                System.out.print("* ");
             }
             System.out.println();
         }
